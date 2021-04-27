2b3ed6f43f4e940eb4878aab984b3ad353b68ce4
lovranitstamas <tamas198601@gmail.com>
2021-04-28 01:03:25 +0200

    create customers model with migration file

---
+++ database/migrations/2021_04_27_224633_create_customers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            //$table->id();
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+            $table->engine = 'InnoDB';
+
+            $table->bigInteger('id')->autoIncrement();
+            $table->string('name', 50);
+            $table->text('email');
+            $table->string('phone',20)->nullable();
+            $table->string('password', 255);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
